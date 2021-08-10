7de2548dcaf75c6077466fd134e113d27acc5c69
boardguy1024 <boardguy1024@gmail.com>
2021-08-10 13:49:36 +0900

    Initial Commit

---
+++ SwiftUI_AppleMusicAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_AppleMusicAnimation
+//
+//  Created by park kyung seok on 2021/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_AppleMusicAnimation/SwiftUI_AppleMusicAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleMusicAnimationApp.swift
+//  SwiftUI_AppleMusicAnimation
+//
+//  Created by park kyung seok on 2021/08/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleMusicAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
